ea9e6755e212c2a31df859c13acd2934c0edfc2e
Naseem <muhammednaseemtk000@gmail.com>
2026-04-13 20:20:43 +0530

    some changes for this app

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class ThapasyaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.splash,
+      initialRoute: AppRoutes.staffMain,
       routes: AppRoutes.routes,
     );
   }
