68ccdbab84976ed22ebce3729c68c92790e87dd9
Naseem <muhammednaseemtk000@gmail.com>
2026-05-11 14:58:25 +0530

    some changes for the app

--- lib/features/staff/log/widget/class_summary_card.dart
+++ lib/features/staff/log/widget/class_summary_card.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ClassSummaryCard extends StatelessWidget {
   final String txt;
-  const ClassSummaryCard({super.key,required this.txt});
+  const ClassSummaryCard({super.key, required this.txt});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class ClassSummaryCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-           Text(
+          Text(
             txt,
             style: TextStyle(
               fontSize: 16,
@@ -33,24 +33,11 @@ class ClassSummaryCard extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
             ),
             child: const TextField(
-              maxLines: 5,
+              maxLines: 2,
               decoration: InputDecoration(
                 hintText:
                     "Describe today's class activities, topics covered, student progress, any observations...",
                 border: InputBorder.none,
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 10),
-
-          const Align(
-            alignment: Alignment.bottomRight,
-            child: Text(
-              "0 characters",
-              style: TextStyle(
-                fontSize: 12,
-                color: Color(0xFF6B7280),
               ),
             ),
           ),
